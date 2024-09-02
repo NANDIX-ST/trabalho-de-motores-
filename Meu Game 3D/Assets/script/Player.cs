e52711a0f7b2a7495b6ce2676f34d0011455756e
NANDIX-ST <fernando.santos1@escolar.ifrn.edu.br>
2024-09-02 16:13:25 -0300

    parte final 2

--- Meu Game 3D/Assets/script/Player.cs
+++ Meu Game 3D/Assets/script/Player.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public int velocidade = 10;
-
     private Rigidbody rb;
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
+
+        Debug.Log("STARTE");
         TryGetComponent(out rb);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        Debug.Log("UPDATE");
+        float h = Input.GetAxis("Horizontal"); // -1 esquerda, 0 nada, 1 direito
+        float v = Input.GetAxis("Vertical"); // -1 pra tras 
         Vector3 direcao = new Vector3(h, 0, v);
-        rb.AddForce(direcao * velocidade *Time.deltaTime, ForceMode.Impulse );
+        rb.AddForce(direcao * velocidade *Time.deltaTime, ForceMode.Impulse);
     }
 }
