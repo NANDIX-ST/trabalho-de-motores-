58de301a433092e15c9afcde637445bd009a47a8
NANDIX-ST <fernando.santos1@escolar.ifrn.edu.br>
2024-09-09 10:21:58 -0300

    hed

--- Meu Game 3D/Assets/script/Player.cs
+++ Meu Game 3D/Assets/script/Player.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Vector3 = UnityEngine.Vector3;
+
 public class Player : MonoBehaviour
 {
     public int velocidade = 10;
     public int forcapulo = 7;
     public bool noChao;
     Rigidbody rb;
-    
-    
-    
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,20 +31,25 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     
+
         float h = Input.GetAxis("Horizontal"); // -1 esquerda, 0 nada, 1 direito
         float v = Input.GetAxis("Vertical"); // -1 pra tras, 0 nada, 1 pra frente 
         Vector3 direcao = new Vector3(h, 0, v);
-        rb.AddForce(direcao * velocidade *Time.deltaTime, ForceMode.Impulse);
+        rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
 
         if (Input.GetKeyDown(KeyCode.Space) && noChao)
         {
             rb.AddForce(Vector3.up * forcapulo, ForceMode.Impulse);
             noChao = false;
         }
+
         if (transform.position.y <= -10)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
+
 }
+    
+    
+    
